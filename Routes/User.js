cefb44789f8d4b9c5c3dd16e3c577dbef8480a77
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-05-31 23:14:38 +0530

    added user routes

---
+++ Routes/User.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const router=express.Router();
+const auth_controller=require("../Controllers/Auth");
+const reset_controller=require("../Controllers/ResetPassword");
+const auth_middleware=require("../Middlewares/Auth");
+
+router.post("/login",auth_controller.postLogin);
+
+router.post("/signup",auth_controller.postSignup);
+
+router.post("./sendotp",auth_controller.sendOTP);
+
+router.post("/changepassword",auth_controller.postChangePassword);
+
+router.post("/reset-password-token",reset_controller.resetPasswordToken);
+
+router.post("/reset-password",reset_controller.resetPassword);
+
+module.exports=router;
