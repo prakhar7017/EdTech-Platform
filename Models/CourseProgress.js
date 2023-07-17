886c52bf4d1a1403f7d2fc835f8ff428bb1d3f90
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-07-18 04:51:40 +0530

    updated courseprogrss model

--- Models/CourseProgress.js
+++ Models/CourseProgress.js
@@ -5,10 +5,15 @@ const courseProgress=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"Course",
     },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
     completedVideos:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:"SubSection",
     }],
+
 })
 
 module.exports=mongoose.model("CourseProgres",courseProgress);
